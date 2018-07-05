97a6d6705068e4580111828ec2e44b40ae03f169
yaoyecaizi <2453419889@qq.com>
2018-07-05 15:46:46 +0800

    增加复选框CheckBox实例

---
+++ androidStudy12-2/app/src/main/java/xyz/xiaoyaoyou/androidstudy12_2/MainActivity.java
@@ -0,0 +1,48 @@
+package xyz.xiaoyaoyou.androidstudy12_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,CompoundButton.OnCheckedChangeListener {
+
+    private CheckBox cb_one;
+    private CheckBox cb_two;
+    private CheckBox cb_three;
+    private Button btn_send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cb_one = (CheckBox)findViewById(R.id.cb_one);
+        cb_two = (CheckBox)findViewById(R.id.cb_two);
+        cb_three = (CheckBox)findViewById(R.id.cb_three);
+        btn_send = (Button)findViewById(R.id.btn_send);
+
+        cb_one.setOnCheckedChangeListener(this);
+        cb_two.setOnCheckedChangeListener(this);
+        cb_three.setOnCheckedChangeListener(this);
+        btn_send.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton,boolean b) {
+        if(compoundButton.isChecked()) Toast.makeText(this,compoundButton.getText().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        String choose = "";
+        if(cb_one.isChecked()) choose += cb_one.getText().toString() + "";
+        if(cb_two.isChecked()) choose += cb_two.getText().toString() + "";
+        if(cb_three.isChecked()) choose += cb_three.getText().toString() + "";
+        Toast.makeText(this,choose,Toast.LENGTH_SHORT).show();
+    }
+}
